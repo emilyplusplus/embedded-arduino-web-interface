5d79a8c1ec313d10fbe96bec68bc0fdea6129504
Emily Carlsen <emilytcyt@gmail.com>
2019-02-21 09:45:23 -0500

    Get basic serial comms working between Arduino and computer app

--- renderer.js
+++ renderer.js
@@ -1,3 +1,29 @@
+var SerialPort = require('serialport');
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let cmd = document.getElementById("cmd")
+let send = document.getElementById("send")
+let response = document.getElementById("response")
+
+var serialPort = new SerialPort('/dev/cu.usbmodem14101', {
+    baudRate: 9600
+});
+
+// Switches the port into "flowing mode"
+serialPort.on('data', function (data) {
+    response.value += data
+});
+
+// Read data that is available but keep the stream from entering //"flowing mode"
+serialPort.on('readable', function () {
+    response.value += serialPort.read()
+});
+
+send.onclick = function() {
+    console.log(cmd.value)
+    serialPort.write(cmd.value)
+    cmd.value = ''
+}
